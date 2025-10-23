fa47f5f0998cb3d6a5a9ce08013967d23080ea4c
Vansh Soni <sonivanshmaster@gmail.com>
2025-10-23 14:38:10 +0530

    Initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 
+# -----------------------------
+# Base Config
+# -----------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-f1%df@!xd&5iy&n#f*owryejaksd%s+)wyl-=!mafps^qv_%sz'
-
 DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
+# -----------------------------
+# Installed Apps
+# -----------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -15,11 +19,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',  # your app
 ]
 
+# -----------------------------
+# Middleware
+# -----------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # For serving static files in production
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -28,6 +36,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# -----------------------------
+# URLs & Templates
+# -----------------------------
 ROOT_URLCONF = 'portfolio.urls'
 
 TEMPLATES = [
@@ -48,6 +59,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
+# -----------------------------
+# Database
+# -----------------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -55,40 +69,45 @@ DATABASES = {
     }
 }
 
+# -----------------------------
+# Password Validators
+# -----------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
 
+# -----------------------------
+# Internationalization
+# -----------------------------
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# -----------------------------
+# Static Files (CSS, JS, Images)
+# -----------------------------
 STATIC_URL = '/static/'
-import os
 
-# This is where Django will look for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# Development static folder
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-# This is where collectstatic will put files for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Production static folder
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise: compressed & cached static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# -----------------------------
+# Media Files (optional)
+# -----------------------------
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# -----------------------------
+# Default primary key field type
+# -----------------------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
